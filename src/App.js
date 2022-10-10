3d5006b1537861ba2440fa15667bb92b1ce0d98b
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-10 12:02:17 -0500

    Create 'components' and 'redux' folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+      </nav>
+      <div className="main">
+      </div>
+      <div className="footer">
+      </div>          
     </div>
   );
-}
+};
 
 export default App;
