8b591ae70e08d516cf2acf8655ed1c6106f55d23
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-10 23:32:35 -0500

    Add code lines in 'Navbar.js' and 'Home.js' files

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCryptoCurrencies } from '../redux/cryptoreducer';
+import CryptoElementList from './CryptoList';
+
+const Home = () => {
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const cryptos = useSelector((state) => state.crypto.cryptosList);
+
+  useEffect(() => {
+    dispatch(fetchCryptoCurrencies());
+  }, []);
+
+
+
+  useEffect(() => {
+    dispatch(fetchCryptoCurrencies());
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="home">
+      <div className="homeSearch">
+        <input type="text" placeholder="Search" onChange={handleChange} value={search} />
+      </div>
+      <CryptoElementList cryptos={cryptos} search={search} />
+    </div>
+  );
+};
+
+export default Home;
