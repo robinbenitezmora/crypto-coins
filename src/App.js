1bd6848e742cdd2dcf2f89b0f4c60e7e5c0b71c2
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-11 16:04:53 -0500

    Change code lines in 'App.js' and 'index.js' files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import React from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import Crypto from './components/Crypto';
-// import store from './redux/configureStore';
-// import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Crypto from './components/Crypto';
+import store from './redux/configureStore';
+import './App.css';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Provider store={store}>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/crypto/:cryptoId" element={<Crypto />} />
-//         </Routes>
-//       </Provider>
-//     </BrowserRouter>
-//   );
-// }
+function App() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/crypto/:cryptoId" element={<Crypto />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
+}
 
-// export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Crypto from './components/Crypto';
-import store from './redux/configureStore';
 import './index.css';
-// import App from './App';
+import App from './App';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/crypto/:cryptoId" element={<Crypto />} />
-        </Routes>
-      </Provider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
